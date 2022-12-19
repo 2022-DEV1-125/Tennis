4f9bc015bab5ade7cd9bebe96fc193c9c7e33257
unknown <pavithra.karunakaran0101@gmail.com>
2022-12-19 20:42:42 +0530

    player A win test implementation

--- lib/src/main/java/Kata_Tennis/service/GameService.java
+++ lib/src/main/java/Kata_Tennis/service/GameService.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.stereotype.Service;
 
 import Kata_Tennis.model.GameRequest;
@@ -26,14 +27,14 @@ public class GameService {
 
 		try {
 
-			Field[] gameRequestParams = gameRequest.getClass().getDeclaredFields();
-			for (Field field : gameRequestParams) {
+			Field[] gameRequestFields = gameRequest.getClass().getDeclaredFields();
+			for (Field field : gameRequestFields) {
 
 				String value = field.get(gameRequest).toString();
 				if (!isBlankOrNull(value)) {
 					System.out.println("Validating " + field.getName() + " :: " + value);
 					if (!validValues.contains(value.toUpperCase())) {
-						message = message + ", " + field.getName();
+						message = message + " " + field.getName();
 					}
 				}
 			}
@@ -41,8 +42,11 @@ public class GameService {
 			if (!isBlankOrNull(message)) {
 				response.setPlayerA_Score("Love");
 				response.setPlayerB_Score("Love");
-				response.setGameResult("Invalid values for parameters :: " + message);
+				response.setGameResult("Invalid values for parameters :: " + message + " Kindly enter valid values");
+			} else {
+				response = calculateScore(gameRequestFields, gameRequest);
 			}
+
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
@@ -52,8 +56,68 @@ public class GameService {
 		return response;
 	}
 
+	private GameResponse calculateScore(Field[] gameRequestFields, GameRequest gameRequest)
+			throws IllegalArgumentException, IllegalAccessException {
+
+		int playerA_score = 0;
+		int playerB_score = 0;
+		String message = "";
+		GameResponse result = new GameResponse();
+
+		for (Field field : gameRequestFields) {
+			String value = field.get(gameRequest).toString();
+			if (!isBlankOrNull(value)) {
+				if (playerA_score <= 4 && playerB_score <= 4) {
+					if (value.equalsIgnoreCase(playerA))
+						playerA_score++;
+					else if (value.equalsIgnoreCase(playerB))
+						playerB_score++;
+				}
+			}
+		}
+
+		message = getMessage(playerA_score, playerB_score);
+
+		result.setPlayerA_Score(getScore(playerA_score));
+		result.setPlayerB_Score(getScore(playerB_score));
+		result.setGameResult(message);
+
+		return result;
+
+	}
+
 	public boolean isBlankOrNull(String s) {
 		return (s == null || s.trim().equals(""));
+	}
+
+	private String getScore(int point) {
+
+		switch (point) {
+		case 0:
+			return "Love";
+		case 1:
+			return "15";
+		case 2:
+			return "30";
+		case 3:
+		case 4:
+			return "40";
+		default:
+			return "Love";
+		}
+
+	}
+
+	private String getMessage(int playerA_score, int playerB_score) {
+		String message = "";
+
+		if (playerA_score >= 4 && playerB_score < 4)
+			message = "Player A won the game";
+		else if (playerB_score >= 4 && playerA_score < 4)
+			message = "Player B won the game";
+
+		return message;
+
 	}
 
 }
--- lib/src/test/java/Kata_Tennis/TennisTest.java
+++ lib/src/test/java/Kata_Tennis/TennisTest.java
@@ -21,7 +21,7 @@ class TennisTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		RestAssured.baseURI = "http://localhost";
-		RestAssured.port = 8080;
+		RestAssured.port = 9080;
 	}
 
 	@Test
@@ -36,10 +36,10 @@ class TennisTest {
 		GameRequest request = new GameRequest();
 		request.setPoint1("A");
 		request.setPoint2("a");
-		request.setPoint3("B");
-		request.setPoint4("><>");
-		request.setPoint5("c");
-		request.setPoint6("3er");
+		request.setPoint3("b");
+		request.setPoint4("<>:;");
+		request.setPoint5("3er");
+		request.setPoint6("C");
 		request.setAdvantagePoint("");
 		request.setGamePoint("");
 
@@ -49,7 +49,30 @@ class TennisTest {
 		GameResponse result = ((ResponseOptions<Response>) response).getBody().as(GameResponse.class);
 		assertEquals("Love", result.getPlayerA_Score());
 		assertEquals("Love", result.getPlayerB_Score());
-		assertEquals("Invalid values for parameters :: , point4, point5, point6", result.getGameResult());
+		assertEquals("Invalid values for parameters ::  point4 point5 point6 Kindly enter valid values", result.getGameResult());
+
+	}
+	
+	@Test
+	public void playerAWinTest() throws Exception {
+
+		GameRequest request = new GameRequest();
+		request.setPoint1("A");
+		request.setPoint2("a");
+		request.setPoint3("b");
+		request.setPoint4("B");
+		request.setPoint5("a");
+		request.setPoint6("A");
+		request.setAdvantagePoint("");
+		request.setGamePoint("");
+
+		Response response = given().contentType("application/json").accept("application/json").body(request).when()
+				.post("/kata-tennis").then().statusCode(200).extract().response();
+		response.getBody().print();
+		GameResponse result = ((ResponseOptions<Response>) response).getBody().as(GameResponse.class);
+		assertEquals("40", result.getPlayerA_Score());
+		assertEquals("30", result.getPlayerB_Score());
+		assertEquals("Player A won the game", result.getGameResult());
 
 	}
 }
